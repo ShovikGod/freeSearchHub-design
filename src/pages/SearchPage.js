b108ed038c5761537ad24770ca1196b03c95dfc1
ShovikGod <shovikmetal@gmail.com>
2022-02-04 19:10:02 +0530

    multiple paper cards

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -10,11 +10,11 @@ function SearchPage() {
       <h6 className='text-center text-muted'>Search Results</h6>
       <div className='row mx-5 mb-5 row-cols-1 row-cols-md-3 row-cols-lg-4 g-2 g-lg-3'>
         <PaperCard title="Hello" issn="123456789" />
-        {/* <PaperCard title="Hello" issn="123456789" />
         <PaperCard title="Hello" issn="123456789" />
         <PaperCard title="Hello" issn="123456789" />
         <PaperCard title="Hello" issn="123456789" />
-        <PaperCard title="Hello" issn="123456789" /> */}
+        <PaperCard title="Hello" issn="123456789" />
+        <PaperCard title="Hello" issn="123456789" />
       </div>
     </div>
   );
